8b73d8d00a107ce0e156ff332ebf0d0668060716
joao victor <joaovictor1c2013@gmail.com>
2020-05-02 18:34:07 -0300

    adicao das rotas company delivery e order

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,22 @@
 import { Router } from 'express';
 
+import DeliveryManController from './app/controllers/DeliveryManController';
+import CompanyController from './app/controllers/CompanyController';
+import SessionController from './app/controllers/SessionController';
+import OrdersConrtoller from './app/controllers/OrdersController';
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
+
+routes.post('/deliveryman', DeliveryManController.store);
+routes.post('/company', CompanyController.store);
+
+routes.post('/sessionsDelivery', SessionController.storeDelivery);
+routes.post('/sessionsCompany', SessionController.storeCompany);
+
+routes.use(authMiddleware);
+
+routes.post('/orders', OrdersConrtoller.store);
+routes.get('/orders', OrdersConrtoller.index);
 
 export default routes;
