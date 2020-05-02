2a78dd4fbac662b917672b4318cff9db0bc800e8
joao victor <joaovictor1c2013@gmail.com>
2020-05-02 15:20:57 -0300

    primeiro commit

--- src/app/models/DeliveryMan.js
+++ src/app/models/DeliveryMan.js
@@ -27,10 +27,6 @@ class DeliveryMan extends Model {
     return this;
   }
 
-  static associate(models) {
-    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
-  }
-
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
