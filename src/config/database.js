6ca1d1750e30846005057f2bb2259f5d45fb6037
joao victor <joaovictor1c2013@gmail.com>
2020-05-04 22:57:26 -0300

    env3

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  host: 'ec2-18-233-32-61.compute-1.amazonaws.com',
-  username: 'wdbxmhqqregemb',
-  password: 'b9f652e4fd8e28a9e69f32c0770faf30e3aa3f271d3d2b90e841eb0ade54680a',
-  database: 'da98hc8iecra3d',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
